91812daebf0b17b1c9e587477c9178e5da2f051f
Ton Smit <ton@pa5lr.com>
2014-06-25 09:03:07 +0200

    flask-hello-world

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -6,13 +6,21 @@ from flask import Flask
 #create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
+
 # use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
 
 # define the view using a function, which returns a string
 def hello_world():
-	return "Hello, World"
+	return "Hello, World!!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
 
 # start the development server using the run() method
 if __name__ == "__main__":
